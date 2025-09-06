9e96895f8b5b87c32a5beb3aad8e2f4c33f2a9b3
Socrates <suxiaogang223@icloud.com>
2025-09-06 16:44:41 +0800

    boot

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+# set clang path and cflags
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+# make kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# start qemu
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
